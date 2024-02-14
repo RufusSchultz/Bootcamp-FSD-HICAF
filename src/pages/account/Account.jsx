1d09687c4a749b3be4a882a122fb3d36ee14c4e3
RufusSchultz <rufusschultz@gmail.com>
2024-02-14 13:53:45 +0100

    Created first page of account page.

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -2,6 +2,8 @@ import "./Account.css";
 import Button from "../../components/button/Button.jsx";
 import {useContext} from "react";
 import {AuthContext} from "../../context/AuthContext.jsx";
+import favorites_img from "../../assets/favorites.png";
+import settings_img from "../../assets/preferences.png";
 
 function Account() {
 
@@ -10,17 +12,35 @@ function Account() {
         contextContent.logOutHandler();
     }
 
-    console.log(contextContent.user);
-
     return (
-        <>
-            <h1>Account paginaaaaaaaaaaaaaaa</h1>
-            <Button
-                className={"big_button"}
-                text={"Log out"}
-                onClick={handleLogOutClick}
-            />
-        </>
+        <div className={"account_page"}>
+            <h2>Welcome, {contextContent.user.username}!</h2>
+            <div className={"content_outer_container"}>
+                <div className={"content_inner_container"}>
+                    <img src={favorites_img} alt="Favorites"/>
+                    <Button
+                        className={"big_button"}
+                        text={"Favorites"}
+                    />
+                </div>
+                <div className={"content_inner_container"}>
+                    <img src={settings_img} alt="Settings"/>
+                    <Button
+                        className={"big_button"}
+                        text={"Settings"}
+                    />
+                </div>
+            </div>
+            <div className={"logout_button_wrapper"}>
+                <h3>Want to log out?</h3>
+                <Button
+                    className={"small_button"}
+                    text={"Click me!"}
+                    onClick={handleLogOutClick}
+                />
+            </div>
+
+        </div>
     )
 }
 
